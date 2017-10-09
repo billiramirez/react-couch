0f1fbc54574c269758dde8d6fb359c61e94095b3
Billi Ramirez <ramirezbilly42@outlook.com>
2017-10-09 13:03:06 -0600

    setting up firebase, signup component into my app

---
+++ src/components/SignIn.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+
+class SignIn extends Component
+{
+    render(){
+        return (
+            <h1>SignIn Component</h1>
+        )
+    }
+}
+
+export default SignIn;
---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import * as firebase from 'firebase';
+
+const config = {
+    apiKey: "AIzaSyBRCy3pC-c4b7wtLu6yAMO1bDFoOaSJEz8",
+    authDomain: "goalcoach-385c9.firebaseapp.com",
+    databaseURL: "https://goalcoach-385c9.firebaseio.com",
+    projectId: "goalcoach-385c9",
+    storageBucket: "",
+    messagingSenderId: "997827257417"
+};
+
+export const firebaseApp  = firebase.initializeApp(config);
